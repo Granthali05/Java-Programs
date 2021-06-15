7723c1a682ed13acf9fa37809224dddc71280a4b
Granthali Jadhav <65496264+Granthali05@users.noreply.github.com>
2021-06-15 22:42:10 +0530

    Add files via upload

---
+++ FtoC.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Ftoc{
+	public static void main(String args[]){
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter your Value");
+	double fren=in.nextDouble();
+	double cel=(5.0/9) * (fren-32);
+	System.out.println("Celsius is:"+cel);
+	
+	}
+		}
